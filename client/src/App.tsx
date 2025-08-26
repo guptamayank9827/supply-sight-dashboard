611918d25a375dfe77fd92c1f3b61e3384d7a7db
Mayank Gupta <guptamayank9827@gmail.com>
2025-08-26 02:14:04 -0500

    added top bar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
+
 import TopBar from './components/TopBar';
 import KPICards from './components/KPICards';
 import TrendChart from './components/TrendChart';
 import Filters from './components/Filters';
 import ProductsTable from './components/ProductsTable';
 
+const DEFAULT_RANGE = 7;
+
 function App() {
+  // state
+  const [range, setRange] = useState<number>(DEFAULT_RANGE);
+
   return(
     <div className="min-h-screen">
 
-      <TopBar />
+      <TopBar selectedRange={range} updateRange={setRange} />
 
       <main className="max-w-7xl mx-auto px-4 py-6 space-y-6">
 
--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -1,7 +1,31 @@
-export default function TopBar() {
+type TopBarProps = {
+  selectedRange: number;
+  updateRange: (value:number) => void;
+}
+
+const RANGES = [7, 14, 30];
+
+export default function TopBar(props:TopBarProps) {
+  
   return (
     <header className="sticky top-0 z-10 bg-white/90 backdrop-blur border-b">
-        TopBar Section
+        <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+
+          <div className="text-brandBlue text-3xl font-bold tracking-tight">SupplySight</div>
+
+          <div className="flex gap-2">
+            {RANGES.map(range => (
+              <button
+                key={range}
+                onClick={() => props.updateRange(range)}
+                className={`px-3 py-1.5 rounded-full text-sm border ${props.selectedRange === range ? 'bg-brandBlue text-white' : 'bg-white border-brandBlue hover:bg-slate-100'}`}
+              >
+                {range}d
+              </button>
+            ))}
+          </div>
+
+        </div>
     </header>
   );
 }
