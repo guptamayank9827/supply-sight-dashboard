b5c3e869d32faaae1649945c30e5683db3fbbd4d
Mayank Gupta <guptamayank9827@gmail.com>
2025-08-26 02:13:50 -0500

    added a primary brand color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brandBlue: "#17263e"
+      }
+    },
   },
   plugins: [],
 }
